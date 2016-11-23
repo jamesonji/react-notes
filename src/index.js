a42c938cabcefd21278374fe14560753deb00476
Song Ji <songji0509@gmail.com>
2016-11-22 21:34:59 -0800

    Set up Express working with React-router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/Main/App';
+import Routes from './routes';
+import { browserHistory } from 'react-router';
+
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Routes history={browserHistory} />,
   document.getElementById('root')
 );
