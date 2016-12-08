ffe6b7ba3c59cf80c8a7708cadcf99aafac2e869
Song Ji <songji0509@gmail.com>
2016-12-08 11:39:14 -0800

    Add footer

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -6,14 +6,31 @@ const LandingPage = () => {
         <div className="landing-splash cover bg-left bg-center-l">
           <div className="bg-black-80 pb5 pb6-m pb7-l">
             <div className="tc-l pt5-m pt6-l ph3">
-              <h1 className="f2 f1-l fw2 white-90 mb0 lh-title">This is your super impressive headline</h1>
-              <h2 className="fw1 f3 white-80 mt3 mb4">Now a subheadline where explain your wonderful new startup even more</h2>
-              <a className="f6 no-underline grow dib v-mid bg-blue white ba b--blue ph3 pv2 mb3" href="/">Call to Action</a>
+              <h1 className="f2 f1-l fw2 white-90 mb0 lh-title">Take clean ntoes as developers</h1>
+              <h2 className="fw1 f3 white-80 mt3 mb4">Designed to simplify note taking experience</h2>
+              <a className="f6 no-underline grow dib v-mid bg-blue white ba b--blue ph3 pv2 mb3" href="login/">Login</a>
               <span className="dib v-mid ph3 white-70 mb3">or</span>
-              <a className="f6 no-underline grow dib v-mid white ba b--white ph3 pv2 mb3" href="">Secondary call to action</a>
+              <a className="f6 no-underline grow dib v-mid white ba b--white ph3 pv2 mb3" href="/singup">Sign up for free</a>
             </div>
           </div>
-        </div> 
+        </div>
+        <footer className="pv4 ph3 ph5-m ph6-l mid-gray">
+          <small className="f6 db tc">© 2016 <b className="ttu">By Song Ji </b>, made with ❤️, All Rights Reserved</small>
+          <div className="tc mt3">
+            <a href="http://github.com/jamesonji" 
+              title="github" 
+              target="_blank" 
+              className="f3 dib ph2 link mid-gray dim">
+              <i className="fa fa-github"></i>
+            </a>
+            <a href="http://www.linkedin.com/in/song-ji" 
+              title="linkedin"
+              target="_blank"  
+              className="f3 dib ph2 link mid-gray dim">
+              <i className="fa fa-linkedin-square"></i>
+            </a>
+          </div>
+        </footer> 
       </div>
 
     )
