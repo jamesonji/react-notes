33ced8156c83e1c49b9a8d695a09d58a4fb518fb
Song Ji <songji0509@gmail.com>
2016-12-14 16:56:11 -0800

    Update environment variable

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,9 +1,9 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: REACT_APP_FIREBASE_API_KEY,
-  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 }
 
 firebase.initializeApp(config)
@@ -11,4 +11,4 @@ firebase.initializeApp(config)
 export const ref = firebase.database().ref()
 export const firebaseAuth = firebase.auth
 
-export const BASE_URL = REACT_APP_BASE_URL
+export const BASE_URL = process.env.REACT_APP_BASE_URL
