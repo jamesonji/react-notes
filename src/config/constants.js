d5c43f2e56c2c6c7678ba6dd3ca620b72a6099d3
Song Ji <songji0509@gmail.com>
2016-12-14 16:00:59 -0800

    react-create-app on Heroku

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,9 +1,9 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
 }
 
 firebase.initializeApp(config)
@@ -11,4 +11,4 @@ firebase.initializeApp(config)
 export const ref = firebase.database().ref()
 export const firebaseAuth = firebase.auth
 
-export const BASE_URL = process.env.BASE_URL
+export const BASE_URL = REACT_APP_BASE_URL
