95cfda811ddff717c4e4eaaea15a11ccf15ed573
Song Ji <songji0509@gmail.com>
2016-12-14 19:37:54 -0800

    Add run time environment package

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,13 +1,19 @@
+import React from 'react';
+import runtimeEnv from '@mars/heroku-js-runtime-env';
 import firebase from 'firebase'
+
+const env = runtimeEnv();
+
 const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  apiKey: env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: env.REACT_APP_FIREBASE_DATABASE_URL,
 }
+
 
 firebase.initializeApp(config)
 
 export const ref = firebase.database().ref()
 export const firebaseAuth = firebase.auth
 
-export const BASE_URL = process.env.REACT_APP_BASE_URL
+export const BASE_URL = env.REACT_APP_BASE_URL
